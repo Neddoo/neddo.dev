dc50d7405c4c8cccb35fd85c1e6b976f70ce942a
Sven <svenbisschop@outlook.com>
2021-06-08 15:37:06 +0200

    👨🏼‍💻 Portfolio Change

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -13,13 +13,13 @@ export default function Home() {
         <title>Neddo</title>
       </Head>
       {/*<div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">*/}
-        <div class="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20 fadeIn transform hover:-translate-y-1 hover:scale-110 transition duration-300 ease-in-out">
+        <div class="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20 fadeIn transform hover:-translate-y-1 hover:scale-105 transition duration-300 ease-in-out">
           <div class="flex justify-center md:justify-end -mt-16">
           <a class="w-20 h-20 object-cover rounded-full"/>
           </div>
           <div>
             <h2 class="text-gray-800 text-3xl font-semibold">Personal Website 👨🏼‍💻</h2>
-            <p class="mt-2 text-gray-600">Personal website. Made with Next.JS</p>
+            <p class="mt-2 text-gray-600">Personal website. Made with Next.JS. Styled with TailwindCSS. Deployed using Vercel. <br></br><br></br></p>
           </div>
           <div class="flex justify-end mt-4">
             <a target="_blank" href="https://github.com/Neddoo/neddo.codes" class="text-xl font-medium underline text-black">GitHub Repository</a>
